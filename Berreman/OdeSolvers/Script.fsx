9904d59ae4ba5e62645b37a0a0b1e4683dab821f
kkkmail <kkk@jfcglobal.net>
2018-10-29 03:58:00 -0400

    20181029.01

---
+++ Berreman/OdeSolvers/Script.fsx
@@ -0,0 +1,39 @@
+﻿//===========================================================
+#load "References.fsx"
+//===========================================================
+open Berreman.MathNetNumericsMath
+open OdeSolvers.Solver
+open System.Numerics
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+open MathNet.Numerics.Optimization
+open Microsoft.FSharp.Core
+open System
+
+let printv s (v : #seq<'a>) = 
+    printfn "%s:" s
+    v 
+    |> List.ofSeq
+    |> List.map(fun e -> printfn "    %A" e)
+
+
+let n = 100
+let tEnd = 1000.0
+let odeParams = { OdeParams.defaultValue with endTime = tEnd }
+
+let d t (v : Vector<double>) = 
+    let x = v.ToArray()
+    let x1 = x |> Array.mapi (fun i _ -> x.[if (i + 1) < n then (i + 1) else 0]) |> vector
+    -0.01 * (1.0 + 4.0 * cos(pi * t / 4.0)) * x1
+
+let i = [ for i in 1..n -> double i ] |> vector
+
+#time
+let result = solve odeParams d i
+#time
+
+let f = result |> List.ofArray |> List.rev |> List.head
+printv "i" i
+printv "f" f
+
---
+++ Berreman/OdeSolvers/Solver.fs
@@ -0,0 +1,32 @@
+﻿namespace OdeSolvers
+
+open System.Numerics
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+open MathNet.Numerics.OdeSolvers
+open Microsoft.FSharp.Core
+open System
+
+module Solver = 
+
+    type OdeParams = 
+        {
+            startTime : double
+            endTime : double
+            stepSize : double
+        }
+
+        static member defaultValue =
+            {
+                startTime = 0.0
+                endTime = 10.0
+                stepSize = 0.01
+            }
+
+
+    let solve (o : OdeParams) f i = 
+        let d = Func<double, Vector<double>, Vector<double>> (fun t x -> f t x)
+        let n = ((o.endTime - o.startTime) / o.stepSize |> int) + 1
+        let result = RungeKutta.FourthOrder(i, o.startTime, o.endTime, n, d)
+        result
