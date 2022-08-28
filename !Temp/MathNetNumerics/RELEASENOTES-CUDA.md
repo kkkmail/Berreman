861bab4c6d0f0ec4deb0f9ff2ac9967f3157fb55
kkkmail <kkk@jfcglobal.net>
2022-08-28 12:12:28 -0400

    NET 6

--- !Temp/MathNetNumerics/RELEASENOTES-CUDA.md
+++
@@ -1,3 +0,0 @@
-### 0.1.0-alpha - TBA
-* With Nvidia CUDA 7.0.28
-* Initial version
--- Berreman/Analytics/Examples/References.fsx
+++ Berreman/Analytics/Examples/References.fsx
@@ -5,14 +5,10 @@
 //===========================================================
 #r "../bin/Debug/net6.0/MathNet.Numerics.dll"
 #r "../bin/Debug/net6.0/MathNet.Numerics.FSharp.dll"
-//#r "../../packages/System.ValueTuple.4.5.0/lib/net47/System.ValueTuple.dll"
 //===========================================================
 #r "../bin/Debug/net6.0/Berreman.dll"
 #r "../bin/Debug/net6.0/Analytics.dll"
 #r "../bin/Debug/net6.0/OpticalProperties.dll"
 //===========================================================
-//#r "../../packages/FSharp.Charting.2.1.0/lib/net45/FSharp.Charting.dll"
-//#r "../../packages/XPlot.Plotly.1.5.0/lib/net45/XPlot.Plotly.dll"
-//#r "../../packages/FSharp.Plotly.1.1.21/lib/net47/FSharp.Plotly.dll"
 #r "nuget: Plotly.NET"
 //===========================================================
